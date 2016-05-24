77e79833e839b77437655d16c869e9174d7a4bee
Simon Lacey <slacey@sileads.com>
2016-05-24 16:14:54 -0400

    foundation sass training

--- FrontEndStore/src/assets/js/app.js
+++ FrontEndStore/src/assets/js/app.js
@@ -1,1 +1,16 @@
 $(document).foundation();
+
+$(window).on('changed.zf.mediaquery', function(event, newSize, oldSize) {
+    if(newSize === 'small' && oldSize === 'medium') {
+        $('#main-menu ul').removeClass('expanded')
+    }
+
+    if(newSize === 'medium' && oldSize === 'small') {
+        $('#main-menu ul').addClass('expanded')
+    }
+});
+
+var size = Foundation.MediaQuery.current;
+if(size === 'small') {
+    $('#main-menu ul').removeClass('expanded')
+}
