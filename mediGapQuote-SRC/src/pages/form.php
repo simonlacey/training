7a2b2e1ab6786d71c09108ad0844bfc0ed16f29a
Simon Lacey <slacey@sileads.com>
2016-07-12 16:34:15 -0400

    remove old folders

--- mediGapQuote-SRC/src/pages/form.php
+++
@@ -1,9 +0,0 @@
----
-company: MediGapQuote
-web: www.mediagapquote.org
-domain: mediagapquote.org
-description: Affordable Medicare Insurance
----
-{{> preloader}}
-{{> form-medicare}}
-{{> hidden-frame}}
--- mediGapQuote-SRC/src/pages/thank-you.php
+++
@@ -1,8 +0,0 @@
----
-company: MediGapQuote
-web: www.mediagapquote.org
-domain: mediagapquote.org
-description: Affordable Medicare Insurance
----
-
-{{> thank-you}}
