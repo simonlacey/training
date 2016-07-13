83989ae7633cecb0f3ef41c722f5c801cb0f08fa
Simon Lacey <slacey@sileads.com>
2016-07-13 11:05:21 -0400

    delete

--- americanhealthinsure.com/assets/js/hidden.js
+++
@@ -1,7 +0,0 @@
-function h_conditions() {
-    document.getElementById('previous_conditons').style.display = "none";
-}
-
-function show() {
-    document.getElementById('previous_conditons').style.display = "block";
-}
